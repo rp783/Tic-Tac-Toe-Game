c260eaa5ed7573e35e2c6fa83d8ece7f0f4659cc
rutvik <rp783@njit.edu>
2022-10-03 17:50:04 -0400

    updated database script

--- Database/db.php
+++ Database/db.php
@@ -7,6 +7,32 @@ function OpenCon()
  $db = "sql1.njit.edu";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
+  $sql = "SELECT FirstName, LastName, UserName FROM User"; // Your columns name and table name at the end 
+  $result = $conn->query($sql);
+
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table>";
+      echo "<tr>";
+          echo "<th>FirstName</th>";
+          echo "<th>LastName</th>";
+          echo "<th>UserName</th>";
+          //echo "<th>Password</th>";
+      echo "</tr>";
+    while($row = $result->fetch_assoc()) {    
+      echo "<tr>";
+          echo "<td>" . $row['FirstName'] . "</td>";
+          echo "<td>" . $row['LastName'] . "</td>";
+          echo "<td>" . $row['UserName'] . "</td>";
+          //echo "<td>" . $row['Password'] . "</td>";
+      echo "</tr>";
+
+      }
+  } else {
+    echo "0 results";
+  }
+
  return $conn;
  }
 
