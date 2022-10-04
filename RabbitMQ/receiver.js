01929cf2293dab2127f92fb943963fa0f8354089
aahnik <ap2527@njit.edu>
2022-10-04 13:25:22 -0400

    Edited receiver.js file

--- RabbitMQ/receiver.js
+++ RabbitMQ/receiver.js
@@ -1,28 +1,21 @@
-#!/usr/bin/env node
+var express = require('express');
+var app = express();
+var amqp = require('amqp/callback_api');
 
-var amqp = require('amqplib/callback_api');
+const port = 3001;
 
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+amqp.connect('amqp://localhost', (error, connection) => {
+    connection.createChannel((error, channel) => { 
+        var queue = 'FirstQueue';
 
-        var queue = 'hello';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
+        channel.assertQueue(queue, {durable: false});
+        console.log('Waiting for the Message in ${queue}');
+        channel.consume(queue, (message) => { 
+            console.log('Received ${message.content}');
+        }, {noAck: true});
+        
     });
 });
+
+app.listen(port, () => console.log('App is listening on port ${port}!'))
+
