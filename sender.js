db5a39ba8b35582a9ac5ce45640c754eff2f0e66
Aahnik Patel <Aahnik>
2022-10-13 16:52:08 +0000

    Connection and Script Edit

--- sender.js
+++ sender.js
@@ -3,9 +3,9 @@
 var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
+if (error0) {
+throw error0;
+}
     connection.createChannel(function(error1, channel) {
         if (error1) {
             throw error1;
---
+++ share.js
@@ -0,0 +1,8 @@
+var server = "http://127.0.0.1"; // An install of RabbitMQ reachable from both clients
+var password = "ap28ap";
+var username = "aahnik28";
+var connectionFactory = new ConnectionFactory { 
+	HostName = server, Password = password , Username = username };
+
+
+
