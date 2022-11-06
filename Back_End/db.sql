c8be628a6ef8aec0cde316897dc8a879f3bed016
Dharmesh85 <dbp64@njit.edu>
2022-11-06 23:21:20 +0000

    FrontEnd BackEnd Database Connection

--- Back_End/db.sql
+++ Back_End/db.sql
@@ -1,8 +1,4 @@
 var mysql = require('mysql');
-var express = require('express');
-
-var app = express();
-app.use(express.json());
 
 var con = mysql.createConnection({
   host: "192.168.1.114",
@@ -14,11 +10,6 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-/*
-  var sql = "INSERT INTO UserTable (ID,Username,Password,FirstName,LastName,Email) VALUES ('1', 'Dharmesh85','Dharmeshp85','Dharmesh', 'Patel', 'test1@gmail.com')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Record Updated");
-  });
+
 });
-*/
+
