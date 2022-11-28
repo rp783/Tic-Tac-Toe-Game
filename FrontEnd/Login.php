915520f0b1d032eb5640e50157a03178d54f1a85
Miten Patel <mkp83@njit.edu>
2022-11-28 23:23:32 +0000

    connection created with rabbitmq

--- FrontEnd/Login.php
+++ FrontEnd/Login.php
@@ -1,7 +1,9 @@
 <?php session_start(); 
-require 'nav.php';
+require("./nav.php");
 ?>
 
+
+</head>
 <body>
     <div class="center">
         <h1>Login</h1>
@@ -19,3 +21,34 @@ require 'nav.php';
     </div>
 </body>
 <?php
+require_once('./rmq/path.inc');
+require_once('./rmq/get_host_info.inc');
+require_once('./rmq/rabbitMQLib.php');
+
+$client = new rabbitMQClient("./rmq/login.ini", "testServer");
+if (isset($_SESSION['username']))
+{
+	echo "<meta http-equiv='refresh' content='2;URL=index.php'>";
+}
+
+if (isset($_POST['username']) and isset($_POST['password']))
+{
+	$request = array('username'=>$_POST['username'], 'password'=>$_POST['password'], 'type'=>'login');
+	$response = $client->send_request($request);
+	switch ($response['msg'])
+	{
+		case 'Username or password are invalid. Please retry':
+			echo "<meta http-equiv='refresh' content='2;URL=Login.php'>";
+			exit();
+		case 'Verified credentials':
+			$_SESSION['isVerified'] = true;
+			$_SESSION['username'] = $_POST['username'];
+			unset($_POST);
+			echo "<meta http-equiv='refresh' content='2;URL=Index.php'>";
+			exit();
+	}
+}
+?>
+</html>
+
+
--- FrontEnd/Registration.php
+++ FrontEnd/Registration.php
@@ -1,5 +1,5 @@
-<?php
-require 'nav.php';
+<?php 
+require("./nav.php");
 ?>
 <!-- Required JavaScript Link -->
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -23,6 +23,11 @@ require 'nav.php';
                 <label for="password">Password</label>
 	    </div>
 
+		<div class="textField">
+		<input type="email" name="email" required>
+                <label for="email">Email</label>
+	    </div>
+
             <input type="submit" value="Register">
         </form>
     </div>
@@ -30,3 +35,43 @@ require 'nav.php';
 
 				</body>
 <?php
+require_once('./rmq/path.inc');
+require_once('./rmq/get_host_info.inc');
+require_once('./rmq/rabbitMQLib.php');
+
+$client = new rabbitMQClient("./rmq/register.ini", "testServer");
+if (isset($_POST['firstname']) 
+	and isset($_POST['lastname']) 
+	and isset($_POST['username']) 
+	and isset($_POST['password'])
+	and isset($_POST['email']))
+{
+
+		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		$request = array(
+			'firstname'=>$_POST['firstname'], 
+			'lastname'=>$_POST['lastname'],
+			'username'=>$_POST['username'],
+			'password'=>$password,
+			'email'=>$_POST['email'],
+			'type'=>'register');
+		$response = $client->send_request($request);
+	switch ($response)
+	{
+		case 'created':
+			echo 'Successfully created. Redirecting to login page in 3 seconds';
+			echo "<meta http-equiv='refresh' content='3;URL=./login.php'>";
+			exit();
+		case 'notCreated':
+			echo 'Username already taken. Please re-enter different username in 3 seconds';
+			echo "<meta http-equiv='refresh' content='3;URL=registration.php'>";
+			exit();
+	}
+}
+/*else
+{
+	echo "Please fill in all fields to proceed";
+}*/
+
+?>
+</html>
